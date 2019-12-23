de3cfd649555b31c606e35a0664ec62ab34aff8c
tenma043 <tenten25420@gmail.com>
2019-12-23 19:23:54 +0700

    test

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require('express')
-const serverless = require('serverless-http')
-
 const app = express()
-const router = express.Router()
+const server = require('http').Server(app)
+// const serverless = require('serverless-http')
+// const io = require('socket.io')(server)
 
+const router = express.Router()
+const path = require('path')
+root=path.join(__dirname,'../source/myapp/dist')
+process.env.SILENCE_EMPTY_LAMBDA_WARNING=true
 
 app.use('/.netlify/functions/app',router)
 
-router.get('/',(req,res)=>{
-    console.log("Initial")
-    res.sendfile('./dist/index.html')
+app.get('/',(req,res)=>{
+    res.sendFile('index.html',{root:"./dist"})
 })
 
-module.exports.handler = serverless(app)
+router.get('/',(req,res)=>{
+    res.sendFile('index.html',{root:root})
+})
+
+// io.on('connection',(socket)=>{
+//     console.log('Connected done')
+// })
+
+
+// module.exports.handler = Server(app)
