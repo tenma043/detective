ea815f9e63e389896848d87c695e34732f87cc23
tenma043 <tenten25420@gmail.com>
2019-12-23 19:31:39 +0700

    Add Socket

--- src/app.js
+++ src/app.js
@@ -1,27 +1,28 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
-// const serverless = require('serverless-http')
-// const io = require('socket.io')(server)
-
+const io = require('socket.io')(server)
 const router = express.Router()
 const path = require('path')
-root=path.join(__dirname,'../source/myapp/dist')
 process.env.SILENCE_EMPTY_LAMBDA_WARNING=true
 
 app.use('/.netlify/functions/app',router)
 
+// for local
 app.get('/',(req,res)=>{
     res.sendFile('index.html',{root:"./dist"})
 })
 
+//for product
 router.get('/',(req,res)=>{
+    let root=path.join(__dirname,'../source/myapp/dist')
     res.sendFile('index.html',{root:root})
 })
 
-// io.on('connection',(socket)=>{
-//     console.log('Connected done')
-// })
+server.listen(9500)
 
+io.on('connection',(socket)=>{
+    console.log('Connected done')
+})
 
 // module.exports.handler = Server(app)
