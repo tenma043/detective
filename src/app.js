d7f63f69ee2df3de02ab4caa479f845f21018946
tenma043 <tenten25420@gmail.com>
2019-12-26 04:34:39 +0700

    demo ui

--- src/app.js
+++ src/app.js
@@ -15,40 +15,40 @@ app.listen(9500)
 // for local
 // app.get('/.netlify/functions/app/',(req,res)=>{
 //     let root=path.join(__dirname,'../source/myapp/dist')
+// //     res.sendFile('home.html',{root:root})
+// app.get('/.netlify/functions/app/test3',(req,res)=>{
+//     let root=path.join(__dirname,'../source/myapp/dist')
 //     res.sendFile('home.html',{root:root})
-app.get('/.netlify/functions/app/test3',(req,res)=>{
-    let root=path.join(__dirname,'../source/myapp/dist')
-    res.sendFile('home.html',{root:root})
-// app.get('/',(req,res)=>{
-//     res.sendFile('home.html',{root:"../dist"})
+// // app.get('/',(req,res)=>{
+// //     res.sendFile('home.html',{root:"../dist"})
+// // })
+
+// app.post('/polling',(req,res)=>{
+//     count-=1
+//     res.json({message:count})
+// })
+// //for product
+// app.get('/test2',(req,res)=>{
+//     let root=path.join(__dirname,'../source/myapp/dist')
+//     res.sendFile('home.html',{root:root})
 // })
 
-app.post('/polling',(req,res)=>{
-    count-=1
-    res.json({message:count})
-})
-//for product
-app.get('/test2',(req,res)=>{
-    let root=path.join(__dirname,'../source/myapp/dist')
-    res.sendFile('home.html',{root:root})
-})
+// router.get('/',(req,res)=>{
+//     let root=path.join(__dirname,'../source/myapp/dist')
+//     res.sendFile('home.html',{root:root})
+// })
+// router.get('/test',(req,res)=>{
+//     let root=path.join(__dirname,'../source/myapp/dist')
+//     res.sendFile('home.html',{root:root})
+// })
+// router.get('/set',(req,res)=>{
+//     message = req.body.message
+//     res.json({message:message})
+// })
 
-router.get('/',(req,res)=>{
-    let root=path.join(__dirname,'../source/myapp/dist')
-    res.sendFile('home.html',{root:root})
-})
-router.get('/test',(req,res)=>{
-    let root=path.join(__dirname,'../source/myapp/dist')
-    res.sendFile('home.html',{root:root})
-})
-router.get('/set',(req,res)=>{
-    message = req.body.message
-    res.json({message:message})
-})
-
-router.post('/polling',(req,res)=>{
-    res.json({message:message})
-})
+// router.post('/polling',(req,res)=>{
+//     res.json({message:message})
+// })
 
 
 module.exports = app
