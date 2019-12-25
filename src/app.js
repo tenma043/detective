52db02ee406f495bf50406a1a4435e8dddb62604
tenma043 <tenten25420@gmail.com>
2019-12-26 03:19:35 +0700

    demo ui

--- src/app.js
+++ src/app.js
@@ -4,20 +4,31 @@ const serverless = require('serverless-http')
 const router = express.Router()
 const path = require('path')
 process.env.SILENCE_EMPTY_LAMBDA_WARNING=true
+var count=60
 
 app.use(express.static('../dist'));
 app.use('/.netlify/functions/app',router)
-
+app.listen(9500)
 // for local
 app.get('/',(req,res)=>{
     res.sendFile('home.html',{root:"../dist"})
 })
 
+app.post('/polling',(req,res)=>{
+    count-=1
+    res.json({message:count})
+})
 //for product
 router.get('/',(req,res)=>{
     let root=path.join(__dirname,'../source/myapp/dist')
     res.sendFile('home.html',{root:root})
 })
 
-// module.exports = app
+router.post('/polling',(req,res)=>{
+    count-=1
+    res.json({message:count})
+})
+
+
+module.exports = app
 module.exports.handler = serverless(app)
